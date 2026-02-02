695e2120fb2545faba6a40433e9c3547f0b01950
Jihao You <95151213+jyou2025@users.noreply.github.com>
2026-02-02 17:16:19 -0500

    Update __init__.py

--- python/ira/__init__.py
+++ python/ira/__init__.py
@@ -4,7 +4,7 @@ Impact Range Assessment (IRA)
 A model-based sensitivity interpretability measure for regression modelling.
 """
 
-from .ira import single_ira, repeated_ira
+from .irapy import single_ira, repeated_ira
 
 __all__ = ["single_ira", "repeated_ira"]
 __version__ = "0.1.0"
